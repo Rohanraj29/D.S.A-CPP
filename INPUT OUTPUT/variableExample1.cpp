a78a9f67c4741eb91b8b4080819543ec33840cdd
Rohanraj29 <157391030+Rohanraj29@users.noreply.github.com>
2024-03-11 22:50:36 +0530

    Delete INPUT OUTPUT directory

--- INPUT OUTPUT/variableExample1.cpp
+++
@@ -1,9 +0,0 @@
-//WAP TO PRINT THE VALUE THAT IS STORED IN a VARIABLE
-#include<iostream>
-using namespace std;
-int main()
-{
-    int a=8;
-    cout<<"VALUE IN a VARIABLE IS:- "<<a;
-    return 0;
-}
--- INPUT OUTPUT/variableExample5.cpp
+++
@@ -1,9 +0,0 @@
-//WAP TO STORE NAME IN VARIABLE AND THEN PRINT.
-#include<iostream>
-using namespace std;
-int main()
-{
-string name={"ROHAN RAJ"};
-cout<<"MY NAME IS "<<name;
-return 0;
-}
