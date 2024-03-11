feac35c6a97fdacea426a1e558d47e68e7657f4e
Rohanraj29 <157391030+Rohanraj29@users.noreply.github.com>
2024-03-11 22:37:14 +0530

    Delete INPUT OUTPUT directory

--- INPUT OUTPUT/variableExample4.cpp
+++
@@ -1,9 +0,0 @@
-//WAP TO PRINT CHARACTER IN CHARACTER DATA TYPE.
-#include<iostream>
-using namespace std;
-int main()
-{
-    char alphabets='R';
-    cout<<"CHARACTER IN ALPHABET VARIABLE:- "<<alphabets;
-    return 0;
-}
