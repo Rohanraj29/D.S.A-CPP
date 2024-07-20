7d2079d3b1b7e21967dfcf29d88c9318154fe90b
Rohanraj29 <157391030+Rohanraj29@users.noreply.github.com>
2024-07-20 14:30:50 +0530

    Add files via upload

---
+++ WORKSHEET 1/questionNo7.cpp
@@ -0,0 +1,11 @@
+//  Change the second element of the array arr to 25 and print the updated array.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={10,20,30,40,50};
+    cout<<"2nd element of an array are:-"<<arr[1]<<endl;
+    arr[1]=25;
+     cout<<"After updation 2nd element of an array are:-"<<arr[1];
+     return 0;
+}
